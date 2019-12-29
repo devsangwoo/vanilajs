6e8e7aa6fe7f09f850e382dba17dd22d471ff99c
sangwoo <dev.sangwoo@gmail.com>
2019-12-29 17:48:10 +0900

    ES6 rest+spread+distructuringe

--- spread.js
+++ spread.js
@@ -5,18 +5,18 @@ const fullWeek = [...firstWeek, ...secondWeek, 'sat', 'sun'];
 
 console.log(fullWeek);
 
-const lastName = prompt('Last name');
-
+//const lastName = prompt('Last name');
+const lastName = 'dd';
 const user = {
   username: 'sangwoo',
   age: 28,
   //...은 언팩킹으로서 express을 열어줌. 그다음 lastName이 empty string이 아니고, lastname: lastname일때 True를 반환
   //Lastname만 쓸수있는것은 Shorthand property의 덕임.
   //{}로 감싸주는것은 Spread가 객체로 받기때문임.
+  password: 'abc123123',
 
   ...(lastName !== '' && { lastName }),
 };
-console.log(user);
 
 //rest 연습
 //parameter란 매개변수로 함수에게 전달해줄 인자들을 말함.
@@ -29,3 +29,20 @@ const bestfriendMaker = (firstOne, ...rest) => {
   console.log(rest);
 };
 bestfriendMaker('cho', 'kim', 'xia');
+//속성 추가하기(디스트럭쳐링+유저)
+const setCountry = ({ country = 'KR', ...rest }) => ({ country, ...rest });
+const country = setCountry(user);
+//속성 이름 변경하기
+const reName = ({ age: Age, ...rest }) => ({ Age, ...rest });
+
+//destructuring은 항상 괄호로 묶어줘야함. 변수명이 같아야함
+const killPassword = ({ password, ...rest }) => rest;
+const cleanUser = killPassword(user);
+console.log(`패스워드가 지워졌습니다.`);
+console.log(cleanUser);
+console.log('나라가 추가되었습니다.');
+console.log(country);
+console.log('age 속성명이 바뀌었습니다.');
+console.log(reName(user));
+
+//forEachs는 각각의 엘리먼트에 특정이벤트를 부여함
